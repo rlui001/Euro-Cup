941ce73f231ca17b69e4468ffb6ccb55e40048cb
Ronson Lui <rlui001@ucr.edu>
2022-01-19 21:24:08 -0800

    fix q8

--- sql_q8_sol.sql
+++ sql_q8_sol.sql
@@ -2,17 +2,9 @@
 -- with the highest number of penalty shots, and which countries 
 -- played that match.
 
-WITH max_penalty AS 
-(SELECT match_no
-FROM match_details
-WHERE penalty_score = (
-SELECT MAX(penalty_score) FROM match_details)
-)
-
-SELECT match_no, m.team_id, country_name
-FROM match_details AS m
-INNER JOIN max_penalty
-USING (match_no)
-INNER JOIN soccer_country
-ON m.team_id = country_id;
-
+SELECT c.country_name, COUNT(*) AS penalty_shots
+FROM soccer_country as c, penalty_shootout AS p
+WHERE c.country_id = p.team_id
+GROUP BY c.country_name
+ORDER BY penalty_shots desc
+LIMIT 1;
