15fd20d1012ab7148cbb8a7e659f52092d388b23
Ronson Lui <rlui001@ucr.edu>
2022-01-19 20:53:02 -0800

    fix query filter to wins only

--- sql_q2_sol.sql
+++ sql_q2_sol.sql
@@ -1,2 +1,2 @@
 -- Write a SQL query to find the number of matches that were won by penalty shootout.
-SELECT COUNT(*) FROM match_mast WHERE decided_by = 'P';
+SELECT COUNT(*) FROM match_mast WHERE decided_by = 'P' AND results = 'WIN';
