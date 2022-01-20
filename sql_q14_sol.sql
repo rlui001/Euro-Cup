579bfaab798843f5164f2daf0ead4754701bd219
Ronson Lui <rlui001@ucr.edu>
2022-01-19 21:29:45 -0800

    Update sql_q14_sol.sql

--- sql_q14_sol.sql
+++ sql_q14_sol.sql
@@ -2,9 +2,9 @@
 -- number of bookings they made for the entire tournament. 
 -- Sort your answer by the number of bookings in descending order.
 
-SELECT c.referee_name, count(m.match_no)
+SELECT c.referee_name, count(m.match_no) AS bookings
 FROM player_booked b
 INNER JOIN match_mast m ON b.match_no = m.match_no
 INNER JOIN referee_mast r ON m.referee_id = r.referee_id
 GROUP BY referee_name
-ORDER BY count(b.match_no) DESC;
+ORDER BY bookings DESC;
