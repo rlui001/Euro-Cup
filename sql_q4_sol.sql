42960dadbe22a3a8f525edcd841874373bf078e1
Ronson Lui <rlui001@ucr.edu>
2022-01-19 20:59:11 -0800

    fix sql q4

--- sql_q4_sol.sql
+++ sql_q4_sol.sql
@@ -1,4 +1,8 @@
 -- Write a SQL query to compute a list showing the number of 
 -- substitutions that happened in various stages of play 
 -- for the entire tournament
-SELECT COUNT(*) FROM euro_cup_2016.player_in_out;
+SELECT play_half, play_schedule, COUNT(*) AS substitutions 
+FROM euro_cup_2016.player_in_out
+WHERE in_out = 'I'
+GROUP BY play_half, play_schedule
+ORDER BY play_half, play_schedule, substitutions DESC;
