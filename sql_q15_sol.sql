44ef919b744fab51cc05eb91cfb8a858627d0b84
Ronson Lui <rlui001@ucr.edu>
2022-01-19 21:35:05 -0800

    use faster query, update q15

--- sql_q15_sol.sql
+++ sql_q15_sol.sql
@@ -1,18 +1,10 @@
 -- Write a SQL query to find the referees who booked 
 -- the most number of players.
 
-WITH player_bookings AS
-(
-	SELECT match_no, COUNT(DISTINCT player_id) as player_sum
-    FROM player_booked
-    GROUP BY match_no
-)
-
-SELECT referee_id, referee_name, SUM(player_sum) as players
-FROM match_mast
-INNER JOIN referee_mast
-USING (referee_id)
-INNER JOIN player_bookings
-ON match_mast.match_no = player_bookings.match_no
-GROUP BY referee_id, referee_name
-ORDER BY players DESC;
+SELECT r.referee_name, COUNT(*) AS bookings
+FROM match_mast AS m, player_booked AS b, referee_mast AS r
+WHERE b.match_no = m.match_no
+AND m.referee_id = r.referee_id
+GROUP BY r.referee_name
+ORDER BY bookings DESC
+LIMIT 1;
